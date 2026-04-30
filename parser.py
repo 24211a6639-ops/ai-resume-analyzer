ba3e0b386584af52de25d49498c2a29a36fc6e1e
Surya Prakash <suryaprakash22330@gmail.com>
2026-04-30 11:28:13 +0530

    fixed merge conflict properly

--- parser.py
+++ parser.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # parser.py
 
 import PyPDF2
@@ -7,62 +6,25 @@ import docx
 def extract_text(file_path):
     text = ""
 
-    if file_path.endswith(".pdf"):
-        with open(file_path, "rb") as f:
-            reader = PyPDF2.PdfReader(f)
-            for page in reader.pages:
-                text += page.extract_text() or ""
+    try:
+        # PDF
+        if file_path.endswith(".pdf"):
+            with open(file_path, "rb") as f:
+                reader = PyPDF2.PdfReader(f)
+                for page in reader.pages:
+                    text += page.extract_text() or ""
 
-    elif file_path.endswith(".docx"):
-        doc = docx.Document(file_path)
-        for para in doc.paragraphs:
-            text += para.text + "\n"
+        # DOCX
+        elif file_path.endswith(".docx"):
+            doc = docx.Document(file_path)
+            for para in doc.paragraphs:
+                text += para.text + "\n"
+
+        else:
+            return ""
+
+    except Exception as e:
+        print("Error reading file:", e)
+        return ""
 
     return text.lower()
-=======
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-def extract_skills(text):
-    skills_db = [
-        "python","java","machine learning","deep learning",
-        "opencv","sql","pandas","numpy","tensorflow","pytorch",
-        "nlp","data science","flask","streamlit","api"
-    ]
-
-    found = []
-    text = text.lower()
-
-    for skill in skills_db:
-        if skill in text:
-            found.append(skill)
-
-    return found
-
-
-def calculate_match(resume_text, job_text):
-    resume_skills = extract_skills(resume_text)
-    job_skills = extract_skills(job_text)
-
-    matched = set(resume_skills) & set(job_skills)
-
-    if len(job_skills) == 0:
-        return 0, [], []
-
-    score = int((len(matched) / len(job_skills)) * 100)
-
-    missing = list(set(job_skills) - set(resume_skills))
-
-    return score, matched, missing
-
-
-def advanced_match(resume_text, job_text):
-    documents = [resume_text, job_text]
-
-    tfidf = TfidfVectorizer()
-    tfidf_matrix = tfidf.fit_transform(documents)
-
-    similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
-
-    return int(similarity[0][0] * 100)
->>>>>>> 0da3034b1e708a241d4cc0d131ebdb772f0fc205
